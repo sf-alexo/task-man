bf6fe6cbed80af24f1bd4571489ec130112d93fb
Alex Ost <alex.ost@speedandfunction.com>
2023-07-12 12:44:37 -0400

    added Edit Category Popups

--- public/src/ManagerPage.tsx
+++ public/src/ManagerPage.tsx
@@ -7,8 +7,11 @@ import { Category } from '../../src/typeorm/category.entity';
 const ManagerPage = () => {
   const [username, setUsername] = useState<string>('FRIEND');
   const [categories, setCategories] = useState<Category[]>([]);
-  const [showPopup, setShowPopup] = useState<boolean>(false); // State to control the popup visibility
-  const [newCategoryName, setNewCategoryName] = useState<string>(''); // State to store the new category name
+  const [showCreatePopup, setShowCreatePopup] = useState<boolean>(false);
+  const [showEditPopup, setShowEditPopup] = useState<boolean>(false);
+  const [editCategoryId, setEditCategoryId] = useState<number | null>(null);
+  const [newCategoryName, setNewCategoryName] = useState<string>('');
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -49,41 +52,54 @@ const ManagerPage = () => {
       };
 
       await axios.post('http://localhost:3000/categories/create', newCategory);
-      fetchCategories(); // Refresh the categories list
-      setNewCategoryName(''); // Clear the new category name
-      setShowPopup(false); // Close the popup
+      fetchCategories();
+      setNewCategoryName('');
+      setShowCreatePopup(false);
     } catch (error) {
       console.error(error);
     }
   };
 
   const handleEditCategory = (categoryId: number) => {
-    // Implement the logic to open the edit popup and perform the update request
-    console.log('Edit Category:', categoryId);
+    setShowEditPopup(true);
+    setEditCategoryId(categoryId);
+  };
+
+  const handleUpdateCategory = async () => {
+    if (!editCategoryId) return;
+
+    try {
+      const token = localStorage.getItem('token');
+      const decoded = jwt_decode(token || '') as { id: string };
+      const userId = Number(decoded.id);
+
+      const updatedCategory = {
+        name: newCategoryName,
+        userId: userId,
+      };
+
+      await axios.put(`http://localhost:3000/categories/${editCategoryId}`, updatedCategory);
+      fetchCategories();
+      setNewCategoryName('');
+      setShowEditPopup(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleDeleteCategory = (categoryId: number) => {
-    // Implement the logic to delete the category
     console.log('Delete Category:', categoryId);
-  };
-
-  const handleActionsClick = () => {
-    setShowPopup(true);
-  };
-
-  const handleOutsideClick = () => {
-    setShowPopup(false);
   };
 
   return (
     <div>
       <h2>Hello, {username}</h2>
       <h1>Manager Page</h1>
-      <button type="button" onClick={() => setShowPopup(true)}>
+      <button type="button" onClick={() => setShowCreatePopup(true)}>
         Create Category
       </button>
-      {showPopup && (
-        <div className="popup">
+      {showCreatePopup && (
+        <div className="create-popup">
           <input
             type="text"
             value={newCategoryName}
@@ -93,7 +109,7 @@ const ManagerPage = () => {
           <button type="button" onClick={handleCreateCategory}>
             Create
           </button>
-          <button type="button" onClick={() => setShowPopup(false)}>
+          <button type="button" onClick={() => setShowCreatePopup(false)}>
             Cancel
           </button>
         </div>
@@ -103,17 +119,33 @@ const ManagerPage = () => {
           <div className="row" key={category.id}>
             <h3>{category.name}</h3>
             <p>Date: {category.dateCreated.toString()}</p>
-          <div className="actions">
-            <button type="button" onClick={() => handleEditCategory(category.id)}>
-              Edit
-            </button>
-            <button type="button" onClick={() => handleDeleteCategory(category.id)}>
-              Delete
-            </button>
+            <div className="actions">
+              <button type="button" onClick={() => handleEditCategory(category.id)}>
+                Edit
+              </button>
+              <button type="button" onClick={() => handleDeleteCategory(category.id)}>
+                Delete
+              </button>
+            </div>
           </div>
-        </div>
         ))}
       </div>
+      {showEditPopup && (
+        <div className="edit-popup">
+          <input
+            type="text"
+            value={newCategoryName}
+            onChange={(e) => setNewCategoryName(e.target.value)}
+            placeholder="Enter new category name"
+          />
+          <button type="button" onClick={handleUpdateCategory}>
+            Update
+          </button>
+          <button type="button" onClick={() => setShowEditPopup(false)}>
+            Cancel
+          </button>
+        </div>
+      )}
       <button type="button" onClick={handleSignOut}>
         Sign Out
       </button>
