c780a1f2e36e833cf759db067af9f206293cdfcb
Alex Ost <alex.ost@speedandfunction.com>
2023-07-13 10:54:24 -0400

    extended Add task Form

--- public/src/AddTask.tsx
+++ public/src/AddTask.tsx
@@ -8,17 +8,27 @@ const AddTask: React.FC = () => {
   const navigate = useNavigate();
 
   const [taskName, setTaskName] = useState('');
+  const [dateStart, setDateStart] = useState('');
+  const [dateEnd, setDateEnd] = useState('');
 
   const handleTaskNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTaskName(e.target.value);
+  };
+
+  const handleDateStartChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDateStart(e.target.value);
+  };
+
+  const handleDateEndChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDateEnd(e.target.value);
   };
 
   const handleAddTask = async () => {
     try {
       const newTask = {
         name: taskName,
-        dateStart: new Date(),
-        dateEnd: new Date(),
+        dateStart: dateStart,
+        dateEnd: dateEnd,
         taskId: Number(categoryId),
       };
 
@@ -34,13 +44,17 @@ const AddTask: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Add Task</h1>
-      <label htmlFor="taskName">Task Name:</label>
-      <input type="text" id="taskName" value={taskName} onChange={handleTaskNameChange} />
-      <button type="button" onClick={handleAddTask}>Save</button>
-      <button type="button" onClick={handleCancel}>Cancel</button>
-    </div>
+  <div>
+    <h1>Add Task</h1>
+    <label htmlFor="taskName">Task Name:</label>
+    <input type="text" id="taskName" value={taskName} onChange={handleTaskNameChange} />
+    <label htmlFor="dateStart">Start Date:</label>
+    <input type="date" id="dateStart" value={dateStart} onChange={handleDateStartChange} required />
+    <label htmlFor="dateEnd">End Date:</label>
+    <input type="date" id="dateEnd" value={dateEnd} onChange={handleDateEndChange} required />
+    <button type="button" onClick={handleAddTask}>Save</button>
+    <button type="button" onClick={handleCancel}>Cancel</button>
+  </div>
   );
 };
 
