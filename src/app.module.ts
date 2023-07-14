df97193f088f496220a45d2185ab3c238aa5635d
Alex Ost <alex.ost@speedandfunction.com>
2023-07-14 08:28:16 -0400

    updated readme, started with docker files

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import entities from './typeorm';
         host: configService.get('DB_HOST'),
         port: +configService.get<number>('DB_PORT'),
         username: configService.get('DB_USERNAME'),
-        //password: configService.get('DB_PASSWORD'),
+        password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         entities: entities,
         synchronize: true,
